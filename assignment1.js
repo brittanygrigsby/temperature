80390ae541c37554aff92ef4a95556ffb2959561
brittanygrigsby <brittany.grigsby@sdgku.edu>
2025-06-26 18:32:21 -0700

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,27 @@
+// 1. Declare Variables
+let character = "Billy";
+let action = "ran after";
+let object = "a big balloon";
+let place = "his house";
+let day = "a sunny Sunday";
+
+// Using "const"
+const event= "birthday party";
+
+// 2. Create the Story
+console. log("--- Funny Story ---");
+console. log(character + " was at " + place + " on " + day + "."); console. log ("Suddenly, " + character + " saw " + object + " floating by and " + action + " it. ");
+console. log("When he got closer, he saw it was a " + event + " and his friends were there waiting to sing Happy Birthday!");
+
+// 3. Experiment with Different Values
+console. log("--- Different Version of the Story ---");
+
+// Change values to create a different story
+character = "Jane";
+action = "run towards";
+object = "a bounce house";
+place = "the park";
+time = "a Monday evening";
+console. log(character + " was at " + place + " on " + time + "."); console. log("Suddenly " + character + " saw " + object + " and decided to " + action + " it.");
+console. log("As she got closer, she saw it was a " + event + " and everyone yelled surprise!");
+
